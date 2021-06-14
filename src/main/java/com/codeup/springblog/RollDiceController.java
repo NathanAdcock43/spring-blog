e1b465d2fc0499c84be14db27836b657ace34d5e
Nathan Adcock <nathan.adcock43@gmail.com>
2021-06-14 10:36:27 -0500

    Feat: Changes to dice roll

--- src/main/java/com/codeup/springblog/RollDiceController.java
+++ src/main/java/com/codeup/springblog/RollDiceController.java
@@ -7,26 +7,24 @@ import org.springframework.web.bind.annotation.PathVariable;
 import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
-public class Lab5 {
     @Controller
-    public class DiceRollController {
-        @GetMapping("/roll-dice/{guess}")
+    public class RollDiceController {
+        @GetMapping("/rollDice/{guess}")
         public String rollDice(@PathVariable int guess, Model model) {
-            //Find a random number between 1 and 6.
             int randomNum = ThreadLocalRandom.current().nextInt(1, 6 + 1);
-            //Compare the guess to the random number.
-            //Store if they guessed correctly in and attribute.
+
             model.addAttribute("isCorrectGuess", randomNum == guess);
             model.addAttribute("myGuess", guess);
             model.addAttribute("randomNumber", randomNum);
-            return "roll-dice";
+            return "rollDice";
         }
 
-        @GetMapping("/roll-dice")
+        @GetMapping("/rollDice")
         public String displayRollDice() {
-            return "roll-dice";
+            return "rollDice";
         }
     }
+
 
 
 
